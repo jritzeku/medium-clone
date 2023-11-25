4edb791ccaa2519819e2e407e7c5becc98ec7c96
Jigme Ritzekura <jigmeritzekura@Jigmes-MacBook-Air.local>
2023-11-24 19:24:37 -0600

    updated socket connection url...should not be pointing to localhost during deployment

--- frontend/src/realtimeCommunication/socketConnection.js
+++ frontend/src/realtimeCommunication/socketConnection.js
@@ -9,23 +9,19 @@ let socket = null
 export const connectWithSocketServer = (userAuth) => {
   const jwtToken = userAuth?.token
 
- 
-  socket = io('http://localhost:5001', {
+  socket = io('https://medium-clone-cxni.onrender.com/5001', {
     auth: {
       token: jwtToken,
-  
     },
   })
- 
 
   socket.on('connect', () => {
- //   console.log('successfully connected with socket.io server')
+    //   console.log('successfully connected with socket.io server')
   })
 
   socket.on('notification', (data) => {
     const { notification } = data
 
-  
     store.dispatch(setNewNotification(notification))
   })
 }
