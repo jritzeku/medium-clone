67d7a6aa50cfd13f58918331589b3b6b137fe69a
Jigme Ritzekura <jigmeritzekura@Jigmes-MacBook-Air.local>
2023-11-27 18:09:19 -0600

    reverted back to formik for Login comp.

--- frontend/src/components/forms/Login.js
+++ frontend/src/components/forms/Login.js
@@ -1,16 +1,14 @@
-
 // @ts-nocheck
 import Modal from 'components/modal/Modal'
+import React, { useEffect, useState } from 'react'
 import { useFormik } from 'formik'
-import { useHistory } from 'react-router-dom'
-import { useDebouncedCallback } from 'use-debounce'
+import { Link, Route, useHistory } from 'react-router-dom'
 
-import { useDispatch, useSelector } from 'react-redux'
+// import { Navigate, redirect, useNavigate } from 'react-router-dom'
 import { Redirect } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import * as yup from 'yup'
 import { loginUserAction } from '../../redux/slices/userSlices'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
 
 const formSchema = yup.object({
   email: yup.string().required('Email is required'),
@@ -21,28 +19,28 @@ const Login = ({ open, onClose, setOpenRegister }) => {
   const dispatch = useDispatch()
   const history = useHistory()
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(formSchema),
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+      // @ts-ignore
+      dispatch(loginUserAction(values))
+      history.push('/')
+    },
   })
 
-  const submitForm = (data) => {
-    dispatch(loginUserAction(data))
-    history.push('/')
-  }
-
+  // @ts-ignore
   const store = useSelector((state) => state?.user)
 
   const { userAuth, loading, serverErr, appErr } = store
 
   if (userAuth) return <Redirect to={`/profile/${userAuth?._id}`} />
 
-  
   return (
-    <Modal open={open} onClose={onClose} shadowBottom={true}>
+    <Modal open={open} onClose={onClose}>
       <img
         onClick={onClose}
         src='/images/close.png'
@@ -54,30 +52,39 @@ const Login = ({ open, onClose, setOpenRegister }) => {
       <p className='text-[18px] mb-8 font-serif text-center'>Welcome back.</p>
 
       <form
-        onSubmit={handleSubmit(submitForm)}
+        onSubmit={formik.handleSubmit}
         className='flex flex-col items-center justify-center gap-5 bg-white w-[400px] py-8'
       >
         {appErr || serverErr ? (
-          <div className='text-red-700'>
+          <div className='text-red-400'>
             {serverErr} {appErr}
           </div>
         ) : null}
 
         <input
+          value={formik.values.email}
+          onChange={formik.handleChange('email')}
+          onBlur={formik.handleBlur('email')}
+          type='text '
+          placeholder='email'
           className='rounded-full border-[1px] text-[14px] p-2 w-[50%]'
-          type='text'
-          placeholder='Email'
-          {...register('email')}
         />
-        <p>{errors.email?.message}</p>
+        <div className='text-red-400 mb-2'>
+          {formik.touched.email && formik.errors.email}
+        </div>
 
         <input
-          className='rounded-full border-[1px] text-[14px] p-2 w-[50%]'
+          value={formik.values.password}
+          onChange={formik.handleChange('password')}
+          onBlur={formik.handleBlur('password')}
           type='password'
-          placeholder='Password'
-          {...register('password')}
+          placeholder='password'
+          className='rounded-full border-[1px] text-[14px] p-2 w-[50%]'
         />
-        <p>{errors.password?.message}</p>
+
+        <div className='text-red-400 mb-2'>
+          {formik.touched.password && formik.errors.password}
+        </div>
 
         <button
           type='submit'
@@ -107,4 +114,20 @@ const Login = ({ open, onClose, setOpenRegister }) => {
 
 export default Login
 
- 
+/*
+
+-BUG: 
+
+Error: Maximum update depth exceeded. This can happen when a component repeatedly calls setState inside componentWillUpdate or componentDidUpdate. React limits the number of nested updates to prevent infinite loops.
+
+    ->for me, it seems issue is with useNavigatE() 
+
+        https://stackoverflow.com/questions/70818978/maximum-update-depth-exceeded-from-navigate-component-react-router-dom-v6
+
+https://typeofnan.dev/fix-the-maximum-update-depth-exceeded-error-in-react/
+
+    ->I ended up using React router v5 and reverting back to <Redirect/> and works now with no bug
+
+
+
+*/
